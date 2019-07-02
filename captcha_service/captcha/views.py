1c908f6bfb80faf335a44f11c5111de7b1ec983a
mukesh5 <mukeshchugh5@gmail.com>
2019-07-02 16:30:54 +0530

    Added Font for the captcha & created views for displaying it

--- captcha_service/captcha/views.py
+++ captcha_service/captcha/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from PIL import Image
 import random
 import string
 
 from claptcha import Claptcha
+from PIL import Image
 # Create your views here.
 
 def index(request):
-	return HttpResponse(generateRandomCaptcha())
+	generateCaptchaImage()
+	return render(request, 'captcha/index.html')
 
 
 def generateRandomCaptcha(n=6):
 	char_set  = string.ascii_lowercase + string.ascii_uppercase + '0123456789'
 	captcha = (random.choice(char_set) for _ in range(n))
+	return ''.join(captcha)
+	
 
-	generateCaptchaImage(''.join(captcha))
 
-
-def generateCaptchaImage(captcha):
-	c = Claptcha(generateRandomCaptcha(), 'FreeMono.ttf',
+def generateCaptchaImage():
+	c = Claptcha(generateRandomCaptcha(), 'captcha/FreeMono.ttf',
 			resample=Image.BICUBIC, noise=0.3)
 
-	c.write('captcha1.png')
+	c.write('captcha/static/captcha/captcha.png')
 
 
